9087e968f4ed35f96b91ca5e091922bd6d6a7c06
Andrei <akrotkov@gmail.com>
2021-12-11 22:01:29 -0800

    Part 2

--- 2021/sol12.js
+++ 2021/sol12.js
@@ -22,6 +22,36 @@ function find_paths(edges, root) {
   return paths;
 }
 
+function canVisit(counts, node) {
+  if (isUppercase(node)) return true;
+  if ((counts[node] || 0) == 0) return true;
+  for (let i in counts) {
+    if (!isUppercase(i) && counts[i] > 1) return false;
+  }
+  return true;
+}
+
+function count_paths(edges, prefix, counts = {}) {
+
+  let count = 0;
+  let connection = prefix[prefix.length - 1];
+  if (connection == 'end') {
+    console.log(prefix.join(','));
+    return 1;
+  }
+  for (let i of edges[connection]) {
+    if (i == 'start') continue;
+    if (canVisit(counts, i)) {
+      counts[i] = (counts[i] || 0) + 1;
+      prefix.push(i);
+      count += count_paths(edges, prefix, counts);
+      prefix.pop();
+      counts[i] = (counts[i] || 0) - 1;
+    }
+  }
+  return count;
+}
+
 function run() {
   let data = readFileFlat(FILE);
   let edges = {
@@ -35,8 +65,17 @@ function run() {
     edges[to].push(from);
   }
 
-  let paths = find_paths(edges, ['start']);
-  console.log(paths.length);
+  for (let i in edges) {
+    edges[i].sort();
+  }
+
+  //console.log(edges);
+
+  //let paths = find_paths(edges, ['start']);
+  //console.log(paths.length);
+
+  let count = count_paths(edges, ['start']);
+  console.log(count);
 }
 
 function readFileFlat(file) {
